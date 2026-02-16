9ccceaafd68603c9c9b7097db871cd926ee732fb
gibbz00 <gabrielhansson00@gmail.com>
2026-02-16 10:57:24 +0100

    refactor: simplify include headers

--- include/simple_http.h
+++ include/simple_http.h
@@ -1,5 +1,4 @@
-#ifndef _SIMPLE_HTTP_H_
-#define _SIMPLE_HTTP_H_
+#pragma once
 
 #include <algorithm>
 #include <atomic>
@@ -29,29 +28,11 @@
 #include <vector>
 
 #include <nghttp2/nghttp2.h>
-
-#include <boost/asio.hpp>
-#include <boost/asio/as_tuple.hpp>
-#include <boost/asio/awaitable.hpp>
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/detached.hpp>
-#include <boost/asio/dispatch.hpp>
 #include <boost/asio/experimental/awaitable_operators.hpp>
 #include <boost/asio/experimental/concurrent_channel.hpp>
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/post.hpp>
+#include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
-#include <boost/asio/ssl/context.hpp>
-#include <boost/asio/ssl/stream.hpp>
-#include <boost/asio/this_coro.hpp>
-#include <boost/asio/use_awaitable.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/http/empty_body.hpp>
-#include <boost/beast/http/message.hpp>
-#include <boost/beast/http/message_fwd.hpp>
-#include <boost/beast/http/string_body_fwd.hpp>
-#include <boost/beast/http/verb.hpp>
+#include <boost/beast.hpp>
 
 namespace simple_http
 {
@@ -1523,16 +1504,21 @@ class HttpServer final
                                                                  version));
             }
         };
+
+
         auto send_response = [](auto socket,
                                 auto http1_ch,
                                 Version version,
                                 auto deadline,
                                 auto max_idle_time) -> asio::awaitable<void> {
-            for (;;)
+            while (true)
             {
                 *deadline = std::chrono::steady_clock::now() + max_idle_time;
+
+
                 auto [ec, h1_rsp] = co_await http1_ch->async_receive(
                     asio::as_tuple(asio::use_awaitable));
+
                 if (ec)
                 {
                     break;
@@ -1572,7 +1558,9 @@ class HttpServer final
                 if (version == Version::Http1)
                     break;
             }
+            co_return;
         };
+
         auto deadline = std::make_shared<std::chrono::steady_clock::time_point>(
             std::chrono::steady_clock::now());
         co_await (
@@ -2165,6 +2153,4 @@ class HttpsClient final : public HttpClient,
 
 #endif
 
-}  // namespace simple_http
-
-#endif  // _SIMPLE_HTTP_H_
+}
